b31ae6fd6aa4522c2d0d0e518d22befe97c350a9
olgak_tdvqm06 <olgak_tdvqm06@10.100.102.9>
2020-07-21 19:44:32 +0300

    test deploy

--- ProPetsConfigServer/src/main/java/proPets/configServer/ProPetsConfigServerApplication.java
+++ ProPetsConfigServer/src/main/java/proPets/configServer/ProPetsConfigServerApplication.java
@@ -5,9 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
-@EnableDiscoveryClient
 @SpringBootApplication
 @EnableConfigServer
+@EnableDiscoveryClient
+
 public class ProPetsConfigServerApplication {
 
 	public static void main(String[] args) {
